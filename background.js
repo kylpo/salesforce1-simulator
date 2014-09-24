71c7b24eaa95a551bd57dc6ff9ea5c48a044a563
Kyle Poole <2kylepoole@gmail.com>
2014-09-23 20:58:39 -0700

    [changed] names and added comments. Added some style to README.

--- background.js
+++ background.js
@@ -1,31 +1,36 @@
-var POPOUT_WIDTH = 360;
-var POPOUT_HEIGHT = 640;
+var PHONE_WIDTH = 360;
+var PHONE_HEIGHT = 640;
 var popups = {};
 
+// Performed when user clicks on the icon in their toolbar
 chrome.browserAction.onClicked.addListener(function () {
     chrome.windows.getCurrent(null, function (window) {
+
+        // Resize window to make room for popup, then launch popup
+        chrome.windows.update(window.id, {width: window.width - PHONE_WIDTH}, launchPopup);
+
         function launchPopup() {
             chrome.windows.create({
                 url: 'https://login.salesforce.com/one/one.app',
                 type: "popup",
-                width: POPOUT_WIDTH,
-                height: POPOUT_HEIGHT,
-                left: window.left + window.width - POPOUT_WIDTH
+                width: PHONE_WIDTH,
+                height: PHONE_HEIGHT,
+                left: window.left + window.width - PHONE_WIDTH
             }, function (popup) {
+                // store original window's dimensions for resize when closing popup
                 popups[popup.id] = {id: window.id, width: window.width};
             });
         }
-
-        chrome.windows.update(window.id, {width: window.width - POPOUT_WIDTH}, launchPopup);
     });
 });
 
+// On a popup's close event, resize the window back to where it was unless the dimensions have changed since launching
 chrome.windows.onRemoved.addListener(function (popupId) {
     var windowBeforePopup = popups[popupId];
 
     if (windowBeforePopup != null) {
         chrome.windows.getCurrent(null, function (windowAfterPopup) {
-            if (windowAfterPopup.width === (windowBeforePopup.width - POPOUT_WIDTH)) {
+            if (windowAfterPopup.width === (windowBeforePopup.width - PHONE_WIDTH)) {
                 chrome.windows.update(windowBeforePopup.id, {width: windowBeforePopup.width});
             }
         });
