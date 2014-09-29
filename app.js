2b18a19d04cb234771d9d984389ee33593d73081
Kyle Poole <2kylepoole@gmail.com>
2014-09-28 21:14:31 -0700

    [added] onTop and minimize icons

--- app.js
+++ app.js
@@ -24,39 +24,92 @@ var TITLE_BAR_HEIGHT = 34;
 //    }
 //};
 
+
+
 window.onload = function() {
     var physicalButton = document.getElementById("physical-button");
+    var alwaysOnTopButton = document.getElementById("on-top-button");
+    var minimizeButton = document.getElementById("minimize-window-button");
+    var closeButton = document.getElementById("close-window-button");
+    var iphone5sButton = document.getElementById("iphone-5s-button");
+    var iphone6Button = document.getElementById("iphone-6-button");
+    var iphone6PlusButton = document.getElementById("iphone-6-plus-button");
+    var ipadButton = document.getElementById("ipad-button");
+
     physicalButton.onclick = function () {
         document.getElementById("options").classList.toggle('is-visible');
         physicalButton.classList.toggle('is-active');
     };
 
-    document.getElementById("on-top-button").onclick = function () {
+    alwaysOnTopButton.onclick = function () {
+        alwaysOnTopButton.classList.toggle('is-active');
         var appWindow = chrome.app.window.current();
         appWindow.setAlwaysOnTop(!appWindow.isAlwaysOnTop());
     };
 
-    document.getElementById("minimize-window-button").onclick = function () {
+    minimizeButton.onclick = function () {
         chrome.app.window.current().minimize();
     };
 
-    document.getElementById("close-window-button").onclick = function () {
+    closeButton.onclick = function () {
         chrome.app.window.current().close();
     };
 
-    document.getElementById("iphone-5s-button").onclick = function () {
+    iphone5sButton.onclick = function () {
+        if (iphone5sButton.classList.contains('is-active')) {
+            return;
+        }
+
+        iphone5sButton.classList.add('is-active');
+        iphone6Button.classList.remove('is-active');
+        iphone6PlusButton.classList.remove('is-active');
+        ipadButton.classList.remove('is-active');
+
         chrome.app.window.current().resizeTo(IPHONE_5S_WIDTH, IPHONE_5S_HEIGHT + TITLE_BAR_HEIGHT);
     };
 
-    document.getElementById("iphone-6-button").onclick = function () {
+    iphone6Button.onclick = function () {
+        if (iphone6Button.classList.contains('is-active')) {
+            return;
+        }
+
+        iphone5sButton.classList.remove('is-active');
+        iphone6Button.classList.add('is-active');
+        iphone6PlusButton.classList.remove('is-active');
+        ipadButton.classList.remove('is-active');
+
         chrome.app.window.current().resizeTo(IPHONE_6_WIDTH, IPHONE_6_HEIGHT + TITLE_BAR_HEIGHT);
     };
 
-    document.getElementById("iphone-6-plus-button").onclick = function () {
+    iphone6PlusButton.onclick = function () {
+        if (iphone6PlusButton.classList.contains('is-active')) {
+            return;
+        }
+
+        iphone5sButton.classList.remove('is-active');
+        iphone6Button.classList.remove('is-active');
+        iphone6PlusButton.classList.add('is-active');
+        ipadButton.classList.remove('is-active');
+
         chrome.app.window.current().resizeTo(IPHONE_6_PLUS_WIDTH, IPHONE_6_PLUS_HEIGHT + TITLE_BAR_HEIGHT);
     };
 
-    document.getElementById("ipad-button").onclick = function () {
+    ipadButton.onclick = function () {
+        if (ipadButton.classList.contains('is-active')) {
+            return;
+        }
+
+        iphone5sButton.classList.remove('is-active');
+        iphone6Button.classList.remove('is-active');
+        iphone6PlusButton.classList.remove('is-active');
+        ipadButton.classList.add('is-active');
+
         chrome.app.window.current().resizeTo(IPAD_WIDTH, IPAD_HEIGHT + TITLE_BAR_HEIGHT);
     };
+
+//    function applyActiveClass(element) {
+//        if (element === iphone5sButton) {
+//
+//        }
+//    }
 };
