3c49f73bb61aff69607879f3a53f6459c9b6a4c4
Kyle Poole <2kylepoole@gmail.com>
2014-10-04 14:54:46 -0700

    [added] address-bar which changes webview url, but does not store the preference

--- app.js
+++ app.js
@@ -16,9 +16,14 @@ var R_KEY = 82;
 window.onload = function() {
     var device = document.getElementById('device');
     var physicalButton = document.getElementById('physical-button');
+
     var alwaysOnTopButton = document.getElementById('on-top-button');
     var minimizeButton = document.getElementById('minimize-window-button');
     var closeButton = document.getElementById('close-window-button');
+
+    var addressBarForm = document.getElementById('address-bar');
+    var urlInput = document.getElementById('url-input');
+
     var iphone5sButton = document.getElementById('iphone-5s-button');
     var iphone6Button = document.getElementById('iphone-6-button');
     var iphone6PlusButton = document.getElementById('iphone-6-plus-button');
@@ -28,6 +33,7 @@ window.onload = function() {
 
     physicalButton.addEventListener('click', function () {
         document.getElementById('options').classList.toggle('is-visible');
+        document.getElementById('address-bar').classList.toggle('is-visible');
         document.getElementById('titlebar').classList.toggle('is-visible');
         physicalButton.classList.toggle('is-active');
     });
@@ -105,6 +111,11 @@ window.onload = function() {
         device.classList.add('tablet');
 
         chrome.app.window.current().resizeTo(IPAD_WIDTH, IPAD_HEIGHT);
+    });
+
+    addressBarForm.addEventListener('submit', function() {
+        //TODO: validate urlInput.value
+        webviewElement.setAttribute('src', urlInput.value);
     });
 
     document.addEventListener('keydown', function (event) {
